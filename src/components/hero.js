01e1df4d204926cd18f346239d6067c08f7d2aa5
Myron <myronbyrnes@gmail.com>
2021-01-25 18:35:26 -0500

    services hero update

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,9 +6,7 @@ export default function Hero() {
       <div className="hero__info">
         <h1 className="hero__title">Caring for Seniors at home</h1>
         <p className="hero__text">Finding the best home care service provider can be overwhelming, but you've come to the right place. Learn more about our services and what might be right for you.</p>
-       
           <a className="button" href="tel:+8135487289">Give Us a Call</a>
-        
       </div>
 
       <div className="hero__image">
