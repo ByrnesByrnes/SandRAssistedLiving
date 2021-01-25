e336ae8ff2b906ee3e9fe8d1a279345ea421bec9
Myron <myronbyrnes@gmail.com>
2021-01-25 18:26:39 -0500

    CSS MediaQueries

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AiFillInstagram } from 'react-icons/ai'
 import { BsEnvelopeFill } from 'react-icons/bs'
+import { NavLink as Link} from 'react-router-dom'
+import * as ROUTES from '../constants/routes'
 
 export default function Footer() {
   return (
@@ -19,8 +21,10 @@ export default function Footer() {
 
       </div>
       
-      <a href="#" className="footer__link">Our Services</a>
-      <a href="#" className="footer__link">Our Difference</a>
+      <Link onClick={() => window.scrollTo(0,0)} to={ROUTES.SERVICES} className="footer__link">Our Services</Link>
+      <Link onClick={() => window.scrollTo(0,0)} to={ROUTES.ABOUT} className="footer__link">About Us</Link>
+      <Link onClick={() => window.scrollTo(0,0)} to={ROUTES.ABOUT} className="footer__link">Our Difference</Link>
+      <Link onClick={() => window.scrollTo(0,0)} to={ROUTES.FACILITY} className="footer__link">Our Facility</Link>
      
       <a href="tel:+8135487289" className="footer__link">(813) 548-7289</a>
       <p className="footer__license">S&R Assisted Living Facility License No: AL13236.</p>
--- src/components/staff.js
+++ src/components/staff.js
@@ -1,26 +1,29 @@
 import React from 'react';
 
-export default function Staff({staff}) {
+export default function Staff({ staff }) {
 
   return (
     <div className="staff">
       <h1 className="staff__title">{staff.title}</h1>
       <p className="staff__role">{staff.role}</p>
-      <div className="staff__image">
-        <img src={staff.image} alt="" />
-      </div>
+      <div className="staff__content">
+        <div className="staff__image">
+          <img src={staff.image} alt="" />
+        </div>
 
-      <div className="staff__info">
-        <h4 className="staff__subtitle">My Background</h4>
-        {staff.bio.map(info => (
-          <p className="staff__text">{info}</p>
-        ))}
-      </div>
-      <div className="staff__info">
-        <h4 className="staff__subtitle">Something Else here</h4>
-        {staff.bio.map(info => (
-          <p className="staff__text">{info}</p>
-        ))}
+        <div className="staff__info">
+          <h4 className="staff__subtitle">My Background</h4>
+          {staff.bio.map(info => (
+            <p className="staff__text">{info}</p>
+          ))}
+        </div>
+        <div className="staff__info">
+          <h4 className="staff__subtitle">Something Else here</h4>
+          {staff.bio.map(info => (
+            <p className="staff__text">{info}</p>
+          ))}
+        </div>
+
       </div>
     </div>
   )
